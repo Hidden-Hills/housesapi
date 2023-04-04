cc44f919a510c061b509b1aeecc96b6af825c4b0
Donovan Rivers <donovanriversjr@gmail.com>
2023-04-04 02:20:08 -0400

    third commit

--- apiproject/settings.py
+++ apiproject/settings.py
@@ -58,8 +58,13 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://127.0.0.1:8000'
+    'http://127.0.0.1:8000',
+    'http://planets.pythonanywhere.com'
 ]
+
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
+CORS_ORIGIN_ALLOW_ALL = True   
+
 
 ROOT_URLCONF = 'apiproject.urls'
 
